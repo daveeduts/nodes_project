0bf8c895c8c66b6d6f475c6aef4a20b84cbd7824
dbunaciu <david.bunaciu@gmail.com>
2024-11-29 18:43:24 +0200

    added disclaimer

--- app_pages/page_1.py
+++ app_pages/page_1.py
@@ -10,3 +10,4 @@ def page_1():
     st.session_state.friend_count = st.number_input("How many friends do you want to add?", min_value=1, step=1)
 
     st.button("Next", on_click=st.session_state.go_to_page, args=(2 if st.session_state.group_count > 0 else 3,))
+    st.markdown("<div class='page-1-footnote'> Disclaimer: Data is temporary and is NOT being collected")
