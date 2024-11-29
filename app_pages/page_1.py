88d3e068fb9e2d30c888cea37dccc2eb076c4f90
dbunaciu <david.bunaciu@gmail.com>
2024-11-29 20:15:01 +0200

    fixed logo

--- app_pages/page_1.py
+++ app_pages/page_1.py
@@ -12,10 +12,10 @@ def page_1():
         """,
         unsafe_allow_html=True
     )
-    st.markdown("<div class='page-1-title'>  Build and see your own friendship network!</div>", unsafe_allow_html=True)
+    st.markdown("<div class='page-1-title'>  Build your own friendship network!</div>", unsafe_allow_html=True)
 
     st.session_state.main_character = st.text_input("Enter your name:", "")
-    st.session_state.group_count = st.number_input("In how many groups do you want to split your friends?", min_value=0, step=1)
+    st.session_state.group_count = st.number_input("How many groups should your friends be split into?", min_value=0, step=1)
     st.session_state.friend_count = st.number_input("How many friends do you want to add?", min_value=1, step=1)
 
     st.button("Next", on_click=st.session_state.go_to_page, args=(2 if st.session_state.group_count > 0 else 3,))
